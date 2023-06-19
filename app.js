03d3d70c28e2dfff424bfeab79579a91459849a1
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2023-06-18 20:51:14 -0600

    Agregar tarea

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 require('colors');
-
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const { 
+        inquirerMenu, 
+        pausa, 
+        leerInput
+    } = require('./helpers/inquirer');
 const Tareas = require('./models/tareas');
-
-
 
 const main = async() =>{
 
     let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+    if(tareasDB){
+        //Establecer las tareas
+        tareas.cargarTareasFromArray(tareasDB); 
+
+    }
+   
+
 
     do {
+        //Imprimir el menu
         opt = await inquirerMenu();
-        console.log({ opt });
 
+        switch (opt) {
+            case '1':
+                 const desc = await leerInput('Descripcion:');
+                 tareas.crearTarea(desc);
+            break;
+            case '2':
+                tareas.listadoCompleto();
+                
+            break;
+        }
 
-        console.log(tareas ); 
+        guardarDB(tareas.listadoArr);   
 
         await pausa();
        
